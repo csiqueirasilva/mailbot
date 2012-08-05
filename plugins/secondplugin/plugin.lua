1cc7d0d67cd69f37b5b85a57aa839432587968fb
Caio Siqueira <csiqueirasilva@gmail.com>
2012-08-05 19:41:29 -0300

    Cleanup

--- plugins/secondplugin/plugin.lua
+++
@@ -1,8 +0,0 @@
---[[print("this is the second plugin running!")
-print(type(mailbot.getBody))
-print(mailbot.getBody(-1,"a"))]]--
-
-print(type(mailbot.getBcc()[2].name))
-if mailbot.getBcc()[2].name == "" then
-	print("Empty String")
-end
